5a95da3ff9983a199da3200d91d863dd9c120c43
Bartoszhumel <79085432+Bartoszhumel@users.noreply.github.com>
2021-02-15 11:46:59 +0100

    01 Zmiana metody policz

--- project_git_train.cpp
+++ project_git_train.cpp
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+int Policz(int a,int b)
+{
+    return a+b;
+}
 int main(void) {
     printf("Hello World\n");
+    std::cout << "W pierwszym branchu; 2+3="<< Policz(2,3))<< std::endl;
     return 0;
 }
 
